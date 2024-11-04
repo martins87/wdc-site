8a852bccedd7aab90ebffdfe7cf69804c32575f4
Daniel Martins <martinsda@gmail.com>
2024-11-04 18:16:15 -0300

    add Libertypay logo to clients

--- app/components/Hero/Clients.tsx
+++ app/components/Hero/Clients.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 // import MarraFit from "../../assets/icons/MarraFit.svg";
 import Cinesercla from "../../assets/icons/Cinesercla.png";
 import MarraFit from "../../assets/icons/MarraFit.png";
+import LibertyPay from "../../assets/icons/LibertyPay.png";
 
 const Clients = () => {
   return (
@@ -10,9 +11,10 @@ const Clients = () => {
       <span className="font-[family-name:var(--font-manrope)] text-base">
         Creating impactful experiences for amazing companies like yours:
       </span>
-      <div className="flex gap-10">
+      <div className="flex gap-16">
         <Image src={Cinesercla} alt="Cinesercla" />
         <Image src={MarraFit} alt="Marrafit" />
+        <Image className="opacity-75" src={LibertyPay} alt="LibertyPay" />
       </div>
     </div>
   );
