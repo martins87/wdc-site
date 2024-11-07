ce7770310ec6b6a72917ee3d10c0cae5c64c6424
Daniel Martins <martinsda@gmail.com>
2024-11-07 17:32:51 -0300

    edit section title styling

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -15,11 +15,11 @@ const SectionTitle: FC<SectionTitleProps> = ({
 }) => {
   return (
     <div className="w-full flex flex-col gap-y-4 md:flex-row items-start md:items-center justify-between mb-10 font-[family-name:var(--font-manrope)] text-white">
-      <div className="w-2/5 flex items-start mobile:items-center gap-x-12">
-        <span className="flex gap-3 text-4xl tablet:text-4xl font-extrabold">
-          <span>{inWhite}</span>
-          <span className="text-main_color">{inRed}</span>
-        </span>
+      <div className="md:w-2/5 flex items-start mobile:items-center gap-x-12">
+        <div className="min-w-fit flex gap-3 text-4xl tablet:text-4xl font-extrabold">
+          <span className="min-w-fit">{inWhite}</span>
+          <span className="min-w-fit text-main_color">{inRed}</span>
+        </div>
         <HorizontalLine />
       </div>
       <span className="w-full md:w-3/5 flex flex-1 text-base tablet:text-[20px] md:ml-12">
