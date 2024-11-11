4d9f6fde8572813e20225df91231b609653c04b2
Daniel Martins <martinsda@gmail.com>
2024-11-11 15:07:42 -0300

    add link to home to logo

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import Link from "next/link";
 
 const Logo = () => {
   return (
     <div className="flex flex-1 lg:flex-initial text-white font-[family-name:var(--font-manrope)]">
-      <div className="sm:hidden">
-        <span className="font-bold">Logo</span>
-      </div>
-      <div className="hidden sm:flex">
-        <span className="font-bold">WebDesign</span>Company
-      </div>
+      <Link href="/">
+        <div className="sm:hidden">
+          <span className="font-bold">Logo</span>
+        </div>
+        <div className="hidden sm:flex">
+          <span className="font-bold">WebDesign</span>Company
+        </div>
+      </Link>
     </div>
   );
 };
