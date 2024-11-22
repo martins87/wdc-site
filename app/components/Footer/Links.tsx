562a548fe9ebf43025bce45de0e6472e8a5931ec
Daniel Martins <martinsda@gmail.com>
2024-11-22 13:30:41 -0300

    edit footer text size

--- app/components/Footer/Links.tsx
+++ app/components/Footer/Links.tsx
@@ -4,8 +4,8 @@ const Links = () => {
   return (
     <div className="w-full min-h-32 flex flex-col md:flex-row items-center gap-y-2 justify-center md:justify-between">
       <SectionsLinks />
-      <span className="flex items-center text-custom_gray">
-        All Rights Reserved. Web Design Company
+      <span className="flex items-center text-custom_gray text-sm">
+        All Rights Reserved. Noble Craft WebDesign
       </span>
     </div>
   );
