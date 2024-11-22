b7cfc09f8524e50527e37fb61a9c2cae9e95b33f
Daniel Martins <martinsda@gmail.com>
2024-11-22 13:01:18 -0300

    add new logo

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -1,14 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
+
+import NobleCraftLogo from "../../assets/icons/Logo.png";
 
 const Logo = () => {
   return (
-    <div className="flex flex-1 lg:flex-initial text-white font-[family-name:var(--font-manrope)]">
-      <Link href="/">
-        <div className="sm:hidden">
-          <span className="font-bold">Logo</span>
-        </div>
-        <div className="hidden sm:flex">
-          <span className="font-bold">WebDesign</span>Company
+    <div className="flex flex-1 text-white font-[family-name:var(--font-manrope)]">
+      <Link className="flex flex-col items-center" href="/">
+        <Image
+          className="w-10 sm:w-14 tablet:w-12"
+          src={NobleCraftLogo}
+          alt="crown icon"
+        />
+        <div className="hidden tablet:flex flex-col items-center justify-start mt-0.5 text-white font-[family-name:var(--font-montserrat)] text-sm font-bold tracking-wider">
+          <span>NOBLE</span>
+          <span className="-mt-[6px]">CRAFT</span>
         </div>
       </Link>
     </div>
