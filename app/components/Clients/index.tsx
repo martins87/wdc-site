112d9dd4e3f2fc88eca8cd29cb0942dcc4702b1b
Daniel Martins <martinsda@gmail.com>
2024-12-01 20:57:55 -0300

    fix build

--- app/components/Clients/index.tsx
+++ app/components/Clients/index.tsx
@@ -8,8 +8,10 @@ import Typography from "../Typography";
 const Clients = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-y-6 -mt-32 md:-mt-20 pb-10 md:pb-20 ">
-      <div className="w-4/5 sm:w-full text-xl text-primary_text text-center capitalize font-[family-name:var(--font-poppins)] font-semibold">
-        Trusted by great companies
+      <div className="w-4/5 sm:w-full text-xl text-primary_text text-center capitalize">
+        <Typography font="poppins" weight="semibold">
+          Trusted by great companies
+        </Typography>
       </div>
       <div className="flex flex-col sm:flex-row items-center gap-16 gap-y-10 mt-3">
         <Image className="w-32 tablet:w-44" src={Cinesercla} alt="Cinesercla" />
