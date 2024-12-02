0132508a2719a9d7511819769112954b4220daba
Daniel Martins <martinsda@gmail.com>
2024-12-01 21:30:11 -0300

    edit contact success message styling

--- app/(pages)/contact/SuccessMessage.tsx
+++ app/(pages)/contact/SuccessMessage.tsx
@@ -5,16 +5,16 @@ import Typography from "@/app/components/Typography";
 
 const SuccessMessage = () => {
   return (
-    <div className="w-full p-2 flex justify-start gap-x-3 md:gap-x-6 rounded-3xl bg-main_color/30 mb-4">
-      <div className="p-2 md:p-3 tablet:p-4 flex items-center justify-center rounded-full bg-main_color">
+    <div className="w-full p-2 flex justify-start gap-x-3 md:gap-x-6 rounded-3xl bg-main_color/20 mb-4">
+      <div className="p-4 md:p-3 tablet:p-4 flex items-center justify-center rounded-full bg-main_color">
         <Image
-          className="w-6 md:w-7 tablet:w-8 flex items-center justify-center"
+          className="w-8 flex items-center justify-center"
           src={Check}
           alt="check icon"
         />
       </div>
       <Typography
-        className="w-[85%] flex items-center text-sm md:text-base tablet:text-xl text-primary_text"
+        className="w-[85%] flex items-center text-sm sm:text-base tablet:text-xl text-primary_text"
         font="manrope"
         weight="bold"
       >
