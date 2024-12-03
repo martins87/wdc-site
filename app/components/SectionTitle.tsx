35d20bbdcfc8682860184522755d09448d4af331
Daniel Martins <martinsda@gmail.com>
2024-12-03 06:57:30 -0300

    fix typography 2

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 
-import Typography from "./Typography";
 import { SectionTitleProps } from "../types/SectionTitle";
 
 const SectionTitle: FC<SectionTitleProps> = ({
@@ -10,22 +9,13 @@ const SectionTitle: FC<SectionTitleProps> = ({
 }) => {
   return (
     <div className="w-full flex flex-col gap-y-4 items-start justify-start mb-10 text-primary_text">
-      <Typography
-        className="min-w-fit flex items-start mobile:items-center gap-2 text-3xl tablet:text-4xl"
-        font="montserrat"
-        weight="extrabold"
-      >
-        <Typography className="min-w-fit">{inWhite}</Typography>
-        <Typography className="min-w-fit text-main_color" font="montserrat">
-          {inRed}
-        </Typography>
-      </Typography>
-      <Typography
-        className="w-full flex flex-1 text-base tablet:text-xl text-custom_gray_2/80"
-        font="poppins"
-      >
+      <span className="min-w-fit flex items-start mobile:items-center gap-2 text-3xl tablet:text-4xl font-[family-name:var(--font-montserrat)] font-extrabold">
+        <span className="min-w-fit">{inWhite}</span>
+        <span className="min-w-fit text-main_color">{inRed}</span>
+      </span>
+      <span className="w-full flex flex-1 font-[family-name:var(--font-poppins)] text-base tablet:text-xl text-custom_gray_2/80">
         {description}
-      </Typography>
+      </span>
     </div>
   );
 };
